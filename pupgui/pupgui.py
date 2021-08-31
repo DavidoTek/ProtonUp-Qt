a21d227b52939dd656f087becb7ba25207bc5a8b
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2021-08-31 22:51:31 +0200

    Support other install directories
    Choose between available install directories. Implementation from https://github.com/AUNaseef/protonup/pull/15

--- pupgui/pupgui.py
+++ pupgui/pupgui.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys
+import sys, os
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
@@ -12,6 +12,23 @@ import protonup.api as papi
 APP_NAME = 'ProtonUp-Qt'
 APP_VERSION = '1.4.0'
 PROTONUP_VERSION = '0.1.4'  # same as in requirements.txt
+
+
+POSSIBLE_INSTALL_LOCATIONS = [
+    {'install_dir': '~/.steam/root/compatibilitytools.d/', 'display_name': 'Steam'},
+    {'install_dir': '~/.var/app/com.valvesoftware.Steam/data/Steam/compatibilitytools.d/', 'display_name': 'Steam (Flatpak)'}
+]
+def available_install_directories():
+    """
+    List available install directories
+    Return Type: str[]
+    """
+    available_dirs = []
+    for loc in POSSIBLE_INSTALL_LOCATIONS:
+        install_dir = os.path.expanduser(loc['install_dir'])
+        if os.path.exists(install_dir):
+            available_dirs.append(install_dir)
+    return available_dirs
 
 
 class MainWindow(QMainWindow):
@@ -27,7 +44,15 @@ class MainWindow(QMainWindow):
         self.ui.btnClose.clicked.connect(self.btnCloseClicked)
         self.ui.btnAbout.clicked.connect(self.btnAboutClicked)
 
-        self.ui.comboInstallDirectory.addItem(papi.install_directory())
+        i = 0
+        current_install_dir = papi.install_directory()
+        for install_dir in available_install_directories():
+            self.ui.comboInstallDirectory.addItem(install_dir)
+            if current_install_dir == install_dir:
+                self.ui.comboInstallDirectory.setCurrentIndex(i)
+            i += 1
+
+        self.ui.comboInstallDirectory.currentIndexChanged.connect(self.comboInstallDirectoryCurrentIndexChanged)
 
         self.updateInfo()
         self._available_releases = papi.fetch_releases()    # ToDo: separate thread
@@ -55,6 +80,11 @@ class MainWindow(QMainWindow):
     def btnAboutClicked(self):
         QMessageBox.about(self, 'About ' + APP_NAME + ' ' + APP_VERSION, APP_NAME + ' v' + APP_VERSION + ' by DavidoTek: https://github.com/DavidoTek/ProtonUp-Qt\nGUI for ProtonUp v' + PROTONUP_VERSION + ': https://github.com/AUNaseef/protonup\n\nCopyright (C) 2021 DavidoTek, licensed under GPLv3')
         QMessageBox.aboutQt(self)
+
+    def comboInstallDirectoryCurrentIndexChanged(self):
+        install_dir = papi.install_directory(self.ui.comboInstallDirectory.currentText())
+        self.ui.statusBar.showMessage('Changed install directory to ' + install_dir, timeout=3000)
+        self.updateInfo()
 
     def updateInfo(self):
         # installed versions
