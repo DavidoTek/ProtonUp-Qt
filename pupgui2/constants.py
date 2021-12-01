04ff41a4ab1c249ce41990f54f9769078ce258b5
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2021-12-01 23:38:16 +0100

    Bump version to 2.3.1

--- pupgui2/constants.py
+++ pupgui2/constants.py
@@ -3,7 +3,7 @@ from PySide6.QtCore import QObject, Qt
 from PySide6.QtGui import QColor, QPalette
 
 APP_NAME = 'ProtonUp-Qt'
-APP_VERSION = '2.3.0'
+APP_VERSION = '2.3.1'
 APP_GHAPI_URL = 'https://api.github.com/repos/Davidotek/ProtonUp-qt/releases'
 ABOUT_TEXT = '''\
 GUI for installing/updating Wine/Proton based compatibility tools.
