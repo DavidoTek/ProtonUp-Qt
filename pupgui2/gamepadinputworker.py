f730ba4a540826ca7047d37a2304fec3a739a181
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2021-12-12 00:59:15 +0100

    Gamepad input: Handle exceptions
    Handle exception when no gamepad is plugged in/disconnected.

--- pupgui2/gamepadinputworker.py
+++ pupgui2/gamepadinputworker.py
@@ -12,39 +12,42 @@ class GamepadInputWorker(QThread):
         self.reset_pos = 0
     
     def run(self):
-        while True:
-            events = inputs.get_gamepad()
-            for event in events:
-                if event.code == 'ABS_HAT0Y' or event.code == 'ABS_HAT0X':
-                    if event.state == -1:
-                        self.press_virtual_key.emit(Qt.Key_Tab, Qt.ShiftModifier)
-                    elif event.state == 1:
-                        self.press_virtual_key.emit(Qt.Key_Tab, Qt.NoModifier)
-                
-                elif event.code == 'BTN_SOUTH' and event.state == 1:
-                    self.press_virtual_key.emit(Qt.Key_Space, Qt.NoModifier)
-                elif event.code == 'BTN_EAST' and event.state == 1:
-                    self.press_virtual_key.emit(Qt.Key_Enter, Qt.NoModifier)
-                
-                elif event.code == 'ABS_Y' or event.code == 'ABS_RY':
-                    if event.state > -100 and event.state < 100:
-                        self.reset_pos = True
-                    elif event.state < 0:
-                        if self.reset_pos:
-                            self.press_virtual_key.emit(Qt.Key_Up, Qt.NoModifier)
-                            self.reset_pos = False
-                    elif event.state > 0:
-                        if self.reset_pos:
-                            self.press_virtual_key.emit(Qt.Key_Down, Qt.NoModifier)
-                            self.reset_pos = False
-                elif event.code == 'ABS_X' or event.code == 'ABS_RX':
-                    if event.state > -100 and event.state < 100:
-                        self.reset_pos = True
-                    elif event.state < 0:
-                        if self.reset_pos:
-                            self.press_virtual_key.emit(Qt.Key_Left, Qt.NoModifier)
-                            self.reset_pos = False
-                    elif event.state > 0:
-                        if self.reset_pos:
-                            self.press_virtual_key.emit(Qt.Key_Right, Qt.NoModifier)
-                            self.reset_pos = False
+        try:
+            while True:
+                events = inputs.get_gamepad()
+                for event in events:
+                    if event.code == 'ABS_HAT0Y' or event.code == 'ABS_HAT0X':
+                        if event.state == -1:
+                            self.press_virtual_key.emit(Qt.Key_Tab, Qt.ShiftModifier)
+                        elif event.state == 1:
+                            self.press_virtual_key.emit(Qt.Key_Tab, Qt.NoModifier)
+                    
+                    elif event.code == 'BTN_SOUTH' and event.state == 1:
+                        self.press_virtual_key.emit(Qt.Key_Space, Qt.NoModifier)
+                    elif event.code == 'BTN_EAST' and event.state == 1:
+                        self.press_virtual_key.emit(Qt.Key_Enter, Qt.NoModifier)
+                    
+                    elif event.code == 'ABS_Y' or event.code == 'ABS_RY':
+                        if event.state > -100 and event.state < 100:
+                            self.reset_pos = True
+                        elif event.state < 0:
+                            if self.reset_pos:
+                                self.press_virtual_key.emit(Qt.Key_Up, Qt.NoModifier)
+                                self.reset_pos = False
+                        elif event.state > 0:
+                            if self.reset_pos:
+                                self.press_virtual_key.emit(Qt.Key_Down, Qt.NoModifier)
+                                self.reset_pos = False
+                    elif event.code == 'ABS_X' or event.code == 'ABS_RX':
+                        if event.state > -100 and event.state < 100:
+                            self.reset_pos = True
+                        elif event.state < 0:
+                            if self.reset_pos:
+                                self.press_virtual_key.emit(Qt.Key_Left, Qt.NoModifier)
+                                self.reset_pos = False
+                        elif event.state > 0:
+                            if self.reset_pos:
+                                self.press_virtual_key.emit(Qt.Key_Right, Qt.NoModifier)
+                                self.reset_pos = False
+        except Exception as e:
+            print('Gamepad error:', e)
