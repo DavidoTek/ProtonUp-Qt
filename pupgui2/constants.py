4a374245af2d02819caffbf224c7900a95310243
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2022-03-08 14:16:08 +0100

    Check if STEAM_ROOT contains compat tool folder
    Correctly identify steam root, still won't work if there are two Steam installations which should NOT be the case!
    
    Fix https://github.com/DavidoTek/ProtonUp-Qt/issues/41

--- pupgui2/constants.py
+++ pupgui2/constants.py
@@ -26,7 +26,8 @@ TEMP_DIR = '/tmp/pupgui2.a70200/'
 _POSSIBLE_STEAM_ROOTS = ['~/.local/share/Steam', '~/.steam/root', '~/.steam/steam', '~/.steam/debian-installation']
 _STEAM_ROOT = _POSSIBLE_STEAM_ROOTS[0]
 for steam_root in _POSSIBLE_STEAM_ROOTS:
-    if os.path.exists(os.path.expanduser(steam_root)):
+    ct_dir = os.path.join(os.path.expanduser(steam_root), 'compatibilitytools.d')
+    if os.path.exists(ct_dir):
         _STEAM_ROOT = steam_root
         break
 
