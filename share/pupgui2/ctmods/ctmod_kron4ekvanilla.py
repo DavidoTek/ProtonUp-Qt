cc42d719fbe336a4d563eec694b6993f22b34e84
Heimen Stoffels <vistausss@fastmail.com>
2022-04-23 19:51:22 +0200

    Update ctmod_kron4ekvanilla.py

--- share/pupgui2/ctmods/ctmod_kron4ekvanilla.py
+++ share/pupgui2/ctmods/ctmod_kron4ekvanilla.py
@@ -11,6 +11,7 @@ CT_LAUNCHERS = ['lutris']
 CT_DESCRIPTION = {}
 CT_DESCRIPTION['en'] = '''Compatibility tool "Wine" to run Windows games on Linux. Official version from the WineHQ sources, compiled by Kron4ek.'''
 CT_DESCRIPTION['de'] = '''Kompatibilitätstool "Wine" für Windows-Spiele unter Linux. Offizielle Version aus den WineHQ Quellen, kompiliert von Kron4ek.'''
+CT_DESCRIPTION['nl'] = '''Compatibiliteitshulpmiddel 'Wine' voor het spelen van Windows-games op Linux. Dit is de officiële versie van WineHQ en is gecompileerd door Kron4ek.'''
 CT_DESCRIPTION['pl'] = '''Narzędzie kompatybilności "Wine" do uruchamiania Windowsowych gier na Linuksie. Oficjalna wersja ze źródeł WineHQ, skompilowana przez Kron4ek.'''
 
 
