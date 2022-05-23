b6bb730c7eff3bbe20f87c8f38c55900cafb7a7c
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2022-05-23 22:42:15 +0200

    Add Proton-Tkg
    Close https://github.com/DavidoTek/ProtonUp-Qt/issues/6

---
+++ pupgui2/resources/ctmods/ctmod_protontkg.py
@@ -0,0 +1,149 @@
+# pupgui2 compatibility tools module
+# Proton-Tkg https://github.com/Frogging-Family/wine-tkg-git
+# Copyright (C) 2022 DavidoTek, partially based on AUNaseef's protonup
+
+import os, shutil, tarfile, requests
+from zipfile import ZipFile
+from PySide6.QtCore import *
+
+CT_NAME = 'Proton Tkg'
+CT_LAUNCHERS = ['steam']
+CT_DESCRIPTION = {}
+CT_DESCRIPTION['en'] = '''Custom Proton build for running Windows games, built with the Wine-tkg build system.'''
+CT_DESCRIPTION['de'] = '''Proton-Tkg Build für Windows-Spiele, erstellt mit dem Wine-tkg Build System.'''
+
+
+class CtInstaller(QObject):
+
+    BUFFER_SIZE = 65536
+    CT_URL = 'https://api.github.com/repos/Frogging-Family/wine-tkg-git/releases'
+    CT_INFO_URL = 'https://github.com/Frogging-Family/wine-tkg-git/releases/tag/'
+
+    p_download_progress_percent = 0
+    download_progress_percent = Signal(int)
+
+    def __init__(self):
+        super(CtInstaller, self).__init__()
+        self.p_download_canceled = False
+
+    def get_download_canceled(self):
+        return self.p_download_canceled
+
+    def set_download_canceled(self, val):
+        self.p_download_canceled = val
+
+    download_canceled = Property(bool, get_download_canceled, set_download_canceled)
+
+    def __set_download_progress_percent(self, value : int):
+        if self.p_download_progress_percent == value:
+            return
+        self.p_download_progress_percent = value
+        self.download_progress_percent.emit(value)
+
+    def __download(self, url, destination):
+        """
+        Download files from url to destination
+        Return Type: bool
+        """
+        try:
+            file = requests.get(url, stream=True)
+        except OSError:
+            return False
+
+        self.__set_download_progress_percent(1) # 1 download started
+        f_size = int(file.headers.get('content-length'))
+        c_count = int(f_size / self.BUFFER_SIZE)
+        c_current = 1
+        destination = os.path.expanduser(destination)
+        os.makedirs(os.path.dirname(destination), exist_ok=True)
+        with open(destination, 'wb') as dest:
+            for chunk in file.iter_content(chunk_size=self.BUFFER_SIZE):
+                if self.download_canceled:
+                    self.download_canceled = False
+                    self.__set_download_progress_percent(-2) # -2 download canceled
+                    return False
+                if chunk:
+                    dest.write(chunk)
+                    dest.flush()
+                self.__set_download_progress_percent(int(min(c_current / c_count * 98.0, 98.0))) # 1-98, 100 after extract
+                c_current += 1
+        self.__set_download_progress_percent(99) # 99 download complete
+        return True
+
+    def __fetch_github_data(self, tag):
+        """
+        Fetch GitHub release information
+        Return Type: dict
+        Content(s):
+            'version', 'date', 'download', 'size', 'checksum'
+        """
+        url = self.CT_URL + (f'/tags/{tag}' if tag else '/latest')
+        data = requests.get(url).json()
+        if 'tag_name' not in data:
+            return None
+
+        values = {'version': data['tag_name'], 'date': data['published_at'].split('T')[0]}
+        for asset in data['assets']:
+            if 'proton' in asset['name']:
+                values['download'] = asset['browser_download_url']
+                values['size'] = asset['size']
+        return values
+
+    def is_system_compatible(self):
+        """
+        Are the system requirements met?
+        Return Type: bool
+        """
+        return True
+
+    def fetch_releases(self, count=100):
+        """
+        List available releases
+        Return Type: str[]
+        """
+        tags = []
+        for release in requests.get(self.CT_URL + '?per_page=' + str(count)).json():
+            if 'tag_name' in release:
+                tags.append(release['tag_name'])
+        return tags
+
+    def get_tool(self, version, install_dir, temp_dir):
+        """
+        Download and install the compatibility tool
+        Return Type: bool
+        """
+        data = self.__fetch_github_data(version)
+
+        if not data or 'download' not in data:
+            return False
+
+        destination = temp_dir
+        destination += data['download'].split('/')[-1]
+
+        if not self.__download(url=data['download'], destination=destination):
+            return False
+
+        install_folder = install_dir + 'proton_tkg_' + data['version'].lower()
+        if os.path.exists(install_folder):
+            shutil.rmtree(install_folder)
+        
+        if '.tar.gz' in destination:
+            tarfile.open(destination, "r:gz").extractall(install_dir)
+        elif '.zip' in destination:
+            with ZipFile(destination) as z:
+                os.mkdir(install_folder)
+                z.extractall(install_folder)
+        else:
+            self.__set_download_progress_percent(-1)
+            return False
+
+        self.__set_download_progress_percent(100)
+
+        return True
+
+    def get_info_url(self, version):
+        """
+        Get link with info about version (eg. GitHub release page)
+        Return Type: str
+        """
+        return self.CT_INFO_URL + version
