8eb0432f6ed76d4b2e9b49f240fb375715d467ad
Eamonn Rea <eamonnrea@gmail.com>
2022-11-16 23:05:41 +0000

    Use QTableView in CtInfoDialog (#137)
    * Use QTableView for CtInfoDialog
    
    * remove tr and rename list_games_item_double_clicked
    
    Co-authored-by: DavidoTek <54072917+DavidoTek@users.noreply.github.com>

--- pupgui2/pupgui2ctinfodialog.py
+++ pupgui2/pupgui2ctinfodialog.py
@@ -53,7 +53,7 @@ class PupguiCtInfoDialog(QObject):
 
         self.ui.btnClose.clicked.connect(self.btn_close_clicked)
 
-        self.ui.listGames.itemDoubleClicked.connect(self.list_games_item_double_clicked)
+        self.ui.listGames.cellDoubleClicked.connect(self.list_games_cell_double_clicked)
 
     def update_game_list_steam(self):
         if self.install_loc.get('launcher') == 'steam' and 'vdf_dir' in self.install_loc:
@@ -61,27 +61,31 @@ class PupguiCtInfoDialog(QObject):
             self.ui.txtNumGamesUsingTool.setText(str(len(self.games)))
 
         self.ui.listGames.clear()
-        for game in self.games:
-            self.ui.listGames.addItem(game.get_app_id_str() + ': ' + game.game_name)
+        self.ui.listGames.setRowCount(len(self.games))
+        self.ui.listGames.setHorizontalHeaderLabels([self.tr('AppID'), self.tr('Name')])
+        for i, game in enumerate(self.games):
+            self.ui.listGames.setItem(i, 0, QTableWidgetItem(game.get_app_id_str()))
+            self.ui.listGames.setItem(i, 1, QTableWidgetItem(game.game_name))
 
         self.batch_update_complete.emit(True)
 
     def update_game_list_lutris(self):
         self.ui.listGames.clear()
-        for game in get_lutris_game_list(self.install_loc):
+        self.ui.listGames.setHorizontalHeaderLabels([self.tr('Slug'), self.tr('Name')])
+        for i, game in enumerate(get_lutris_game_list(self.install_loc)):
             if game.runner == 'wine':
                 cfg = game.get_game_config()
                 if self.ctool.displayname == cfg.get('wine', {}).get('version'):
-                    self.ui.listGames.addItem(game.name)
+                    self.ui.listGames.setItem(i, 0, QTableWidgetItem(game.slug))
+                    self.ui.listGames.setItem(i, 1, QTableWidgetItem(game.name))
 
     def btn_close_clicked(self):
         self.ui.close()
 
-    def list_games_item_double_clicked(self, item):
+    def list_games_cell_double_clicked(self, row):
         if self.install_loc.get('launcher') == 'steam':
-            steam_game_id = item.text().split(':')[0]
-            if not steam_game_id == '-1':
-                open_webbrowser_thread(STEAM_APP_PAGE_URL + steam_game_id)
+            steam_game_id = str(self.ui.listGames.item(row, 0).text())
+            open_webbrowser_thread(STEAM_APP_PAGE_URL + steam_game_id)
 
     def btn_batch_update_clicked(self):
         steam_config_folder = self.install_loc.get('vdf_dir')
