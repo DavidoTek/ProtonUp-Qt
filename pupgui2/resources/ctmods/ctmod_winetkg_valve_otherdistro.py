1aef62714b753df40f09fe6d6de3bbf686e2b762
Eamonn Rea <eamonnrea@gmail.com>
2023-02-02 19:28:11 +0000

    ctmod: Remove unused vars from Wine tkg

--- pupgui2/resources/ctmods/ctmod_winetkg_valve_otherdistro.py
+++ pupgui2/resources/ctmods/ctmod_winetkg_valve_otherdistro.py
@@ -2,12 +2,6 @@
 # Proton-Tkg https://github.com/Frogging-Family/wine-tkg-git
 # Copyright (C) 2022 DavidoTek, partially based on AUNaseef's protonup
 
-import os
-import glob
-import shutil
-import tarfile
-from zipfile import ZipFile
-
 from PySide6.QtCore import QCoreApplication, Signal
 
 from pupgui2.resources.ctmods.ctmod_protontkg import CtInstaller as TKGCtInstaller  # Use ProtonTKg Ctmod as base
@@ -21,11 +15,6 @@ CT_DESCRIPTION = {'en': QCoreApplication.instance().translate('ctmod_winetkg_val
 class CtInstaller(TKGCtInstaller):
 
     BUFFER_SIZE = 65536
-    CT_URL = 'https://api.github.com/repos/Frogging-Family/wine-tkg-git/releases'
-    CT_INFO_URL = 'https://github.com/Frogging-Family/wine-tkg-git/releases/tag/'
-    CT_WORKFLOW_URL = 'https://api.github.com/repos/Frogging-Family/wine-tkg-git/actions/workflows'
-    CT_ARTIFACT_URL = 'https://api.github.com/repos/Frogging-Family/wine-tkg-git/actions/runs/{}/artifacts'
-    CT_INFO_URL_CI = 'https://github.com/Frogging-Family/wine-tkg-git/actions/runs/'
     PROTON_PACKAGE_NAME = 'wine-valvexbe'
     TKG_EXTRACT_NAME = 'wine_tkg'
 
