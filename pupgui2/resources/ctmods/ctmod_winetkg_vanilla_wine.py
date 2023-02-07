f15a2031d697f75f82d8f00d344ce5f4defc299e
Eamonn Rea <eamonnrea@gmail.com>
2023-02-07 21:34:49 +0000

    Lutris: Add Vanilla Wine (#181)

---
+++ pupgui2/resources/ctmods/ctmod_winetkg_vanilla_wine.py
@@ -0,0 +1,25 @@
+# pupgui2 compatibility tools module
+# Proton-Tkg https://github.com/Frogging-Family/wine-tkg-git
+# Copyright (C) 2022 DavidoTek, partially based on AUNaseef's protonup
+
+from PySide6.QtCore import QCoreApplication, Signal
+
+from pupgui2.resources.ctmods.ctmod_protontkg import CtInstaller as TKGCtInstaller  # Use ProtonTKg Ctmod as base
+
+
+CT_NAME = 'Wine Tkg (Vanilla Wine)'
+CT_LAUNCHERS = ['lutris', 'advmode']
+CT_DESCRIPTION = {'en': QCoreApplication.instance().translate('ctmod_winetkg_vanilla_ubuntu', '''Custom Wine build for running Windows games, built with the Wine-tkg build system.''')}
+
+
+class CtInstaller(TKGCtInstaller):
+
+    BUFFER_SIZE = 65536
+    PROTON_PACKAGE_NAME = 'wine-ubuntu.yml'
+    TKG_EXTRACT_NAME = 'wine_tkg'
+
+    p_download_progress_percent = 0
+    download_progress_percent = Signal(int)
+
+    def __init__(self, main_window = None):
+        super().__init__(main_window)
