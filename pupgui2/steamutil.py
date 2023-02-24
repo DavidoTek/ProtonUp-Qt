DavidoTek/ProtonUp-Qt

commit cb9b7168157743688cc5a8bc6a463e8d73b7976f

Steam Apps: Don't fetch soundtracks as steamapps (#183)
* Steam Apps: Don't fetch soundtracks as steamapps

* Prevent error if acf doesn't contain AppState

---------

Co-authored-by: DavidoTek <54072917+DavidoTek@users.noreply.github.com>

pupgui2/steamutil.py
@@ -44,9 +44,17 @@ def get_steam_app_list(steam_config_folder: str, cached=False, no_shortcuts=Fals
             if 'apps' not in v.get('libraryfolders').get(fid):
                 continue
             fid_path = v.get('libraryfolders').get(fid).get('path')
+            fid_libraryfolder_path = fid_path
             if fid == '0':
                 fid_path = os.path.join(fid_path, 'steamapps', 'common')
             for appid in v.get('libraryfolders').get(fid).get('apps'):
+                # Skip if app isn't installed to `/path/to/steamapps/common` - Skips soundtracks
+                fid_steamapps_path = os.path.join(fid_libraryfolder_path, 'steamapps')  # e.g. /home/gaben/Games/steamapps
+                appmanifest_path = os.path.join(fid_steamapps_path, f'appmanifest_{appid}.acf')
+                appmanifest_install_path = vdf.load(open(appmanifest_path)).get('AppState', {}).get('installdir')
+                if not os.path.isdir(os.path.join(fid_steamapps_path, 'common', appmanifest_install_path)):
+                    continue
+
                 app = SteamApp()
                 app.app_id = int(appid)
                 app.libraryfolder_id = fid

commit f2ad843198c4eb4133bfeedfd49c965fd492b3a0

Fix Steam games list regression from #183 (#197)
* Fix Steam games list regression from #192

* Ensure installdir exists in appmanifest

pupgui2/steamutil.py
@@ -51,9 +51,10 @@ def get_steam_app_list(steam_config_folder: str, cached=False, no_shortcuts=Fals
                 # Skip if app isn't installed to `/path/to/steamapps/common` - Skips soundtracks
                 fid_steamapps_path = os.path.join(fid_libraryfolder_path, 'steamapps')  # e.g. /home/gaben/Games/steamapps
                 appmanifest_path = os.path.join(fid_steamapps_path, f'appmanifest_{appid}.acf')
-                appmanifest_install_path = vdf.load(open(appmanifest_path)).get('AppState', {}).get('installdir')
-                if not os.path.isdir(os.path.join(fid_steamapps_path, 'common', appmanifest_install_path)):
-                    continue
+                if os.path.isfile(appmanifest_path):
+                    appmanifest_install_path = vdf.load(open(appmanifest_path)).get('AppState', {}).get('installdir', None)
+                    if not appmanifest_install_path or not os.path.isdir(os.path.join(fid_steamapps_path, 'common', appmanifest_install_path)):
+                        continue
 
                 app = SteamApp()
                 app.app_id = int(appid)