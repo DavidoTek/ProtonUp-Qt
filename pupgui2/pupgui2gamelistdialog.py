605a354b1519eec4d230a5cc7ebfc88d61bf71e8
Eamonn Rea <eamonnrea@gmail.com>
2023-02-25 17:02:25 +0000

    Games List: Move Steam-Specific Logic to steamutil  (#198)
    * Games List: Split AWACY and ProtonDB logic into steamutil functions
    
    Cleans up the Games List dialog a bit and also allows for potential re-use of these functions.
    
    * Further logic changes
    
    * Remove btn_fetch_protondb_clicked
    
    * Reduce compat tool combobox logic
    
    * Reduce ProtonDB item logic
    
    * Move AWACY/Compat text methods from steamutil to gamelist
    
    * Revert swapped imports
    
    ---------
    
    Co-authored-by: DavidoTek <54072917+DavidoTek@users.noreply.github.com>

--- pupgui2/pupgui2gamelistdialog.py
+++ pupgui2/pupgui2gamelistdialog.py
@@ -1,7 +1,7 @@
 import os
 import pkgutil
 
-from typing import List, Callable
+from typing import List, Callable, Tuple
 from datetime import datetime
 
 from PySide6.QtCore import QObject, Signal, Slot, QDataStream, QByteArray, Qt
@@ -41,8 +41,7 @@ class PupguiGameListDialog(QObject):
     def load_ui(self):
         data = pkgutil.get_data(__name__, 'resources/ui/pupgui2_gamelistdialog.ui')
         ui_file = QDataStream(QByteArray(data))
-        loader = QUiLoader()
-        self.ui = loader.load(ui_file.device())
+        self.ui = QUiLoader().load(ui_file.device())
 
     def setup_ui(self):
         if self.launcher == 'steam':
@@ -58,13 +57,9 @@ class PupguiGameListDialog(QObject):
         self.ui.tableGames.horizontalHeaderItem(3).setToolTip('https://areweanticheatyet.com')
         self.update_game_list_steam()
 
-        if os.path.exists('/.flatpak-info'):
-            self.ui.lblSteamRunningWarning.setVisible(True)
-            self.ui.lblSteamRunningWarning.setStyleSheet('QLabel { color: grey; }')
-        elif is_steam_running():
-            self.ui.lblSteamRunningWarning.setVisible(True)
-        else:
-            self.ui.lblSteamRunningWarning.setVisible(False)
+        # Only show warning if Steam is running, and make it grey if we're running in Flatpak
+        self.ui.lblSteamRunningWarning.setVisible(is_steam_running() or os.path.exists('/.flatpak-info'))
+        self.ui.lblSteamRunningWarning.setStyleSheet('QLabel { color: grey; }' if os.path.exists('/.flatpak-info') else self.ui.lblSteamRunningWarning.styleSheet())
 
         self.ui.tableGames.setColumnWidth(0, 300)
         self.ui.tableGames.setColumnWidth(3, 70)
@@ -87,7 +82,7 @@ class PupguiGameListDialog(QObject):
     def update_game_list_steam(self):
         """ update the game list for the Steam launcher """
         self.games = get_steam_game_list(steam_config_folder=self.install_loc.get('vdf_dir'))
-        ctools = [c if c != 'SteamTinkerLaunch' else 'Proton-stl' for c in sort_compatibility_tool_names(list_installed_ctools(self.install_dir, without_version=True), reverse=True) ]
+        ctools = [c if c != 'SteamTinkerLaunch' else 'Proton-stl' for c in sort_compatibility_tool_names(list_installed_ctools(self.install_dir, without_version=True), reverse=True)]
         ctools.extend(t.ctool_name for t in get_steam_ctool_list(steam_config_folder=self.install_loc.get('vdf_dir')))
 
         self.ui.tableGames.setRowCount(len(self.games))
@@ -102,14 +97,11 @@ class PupguiGameListDialog(QObject):
 
             # Compat Tool combobox
             combo = QComboBox()
-            combo.addItem('-')
-            combo.addItems(ctools)
+            combo.addItems(['-'] + ctools)
             if game.compat_tool not in ctools:
                 combo.addItem(game.compat_tool)
-            if game.compat_tool is None:
-                combo.setCurrentText('-')
-            else:
-                combo.setCurrentText(game.compat_tool)
+
+            combo.setCurrentText('-' if game.compat_tool is None else game.compat_tool)
             combo.currentTextChanged.connect(lambda text,game=game: self.queue_ctool_change_steam(text, game))
 
             compat_item = QTableWidgetItem()
@@ -120,63 +112,22 @@ class PupguiGameListDialog(QObject):
 
             # ProtonDB status
             btn_fetch_protondb = QPushButton(self.tr('click'))
-            btn_fetch_protondb.clicked.connect(lambda checked=False, game=game: self.btn_fetch_protondb_clicked(game))
+            btn_fetch_protondb.clicked.connect(lambda checked=False, game=game: get_protondb_status(game, self.protondb_status_fetched))
 
-            fetch_protondb_item = QTableWidgetItem()
-            fetch_protondb_item.setData(Qt.DisplayRole, '')  # Shouldn't need to be set, should always be invisible
-
-            self.ui.tableGames.setItem(i, 4, fetch_protondb_item)
+            self.ui.tableGames.setItem(i, 4, QTableWidgetItem())
             self.ui.tableGames.setCellWidget(i, 4, btn_fetch_protondb)
 
-            # SteamDeck compatibility
-            deckc = game.get_deck_compat_category()
-            deckt = game.get_deck_recommended_tool()
-            lbltxt = ''
-            if deckc == SteamDeckCompatEnum.UNKNOWN:
-                lbltxt = self.tr('Unknown')
-            elif deckc == SteamDeckCompatEnum.UNSUPPORTED:
-                lbltxt = self.tr('Unsupported')
-            elif deckc == SteamDeckCompatEnum.PLAYABLE:
-                if deckt == '':
-                    lbltxt = self.tr('Playable')
-                elif deckt == 'native':
-                    lbltxt = self.tr('Native (playable)')
-                else:
-                    lbltxt = self.tr('Playable using {compat_tool}').format(compat_tool=deckt)
-            elif deckc == SteamDeckCompatEnum.VERIFIED:
-                if deckt == '':
-                    lbltxt = self.tr('Verified')
-                elif deckt == 'native':
-                    lbltxt = self.tr('Native (verified)')
-                else:
-                    lbltxt = self.tr('Verified for {compat_tool}').format(compat_tool=deckt)
-
+            lbltxt = self.get_steamdeck_compatibility(game)
             self.ui.tableGames.setItem(i, 2, QTableWidgetItem(lbltxt))
 
             # AWACY status
             lblicon = QLabel()
             p = QPixmap()
-            if game.awacy_status == AWACYStatus.ASUPPORTED:
-                lblicon.setToolTip(self.tr('Support was explicitly enabled / works out of the box'))
-                p.loadFromData(pkgutil.get_data(__name__, 'resources/img/awacy_supported.png'))
-            elif game.awacy_status == AWACYStatus.PLANNED:
-                lblicon.setToolTip(self.tr('Game plans to support Proton/Wine'))
-                p.loadFromData(pkgutil.get_data(__name__, 'resources/img/awacy_planned.png'))
-            elif game.awacy_status == AWACYStatus.RUNNING:
-                lblicon.setToolTip(self.tr('No official statement but runs fine (may require tinkering)'))
-                p.loadFromData(pkgutil.get_data(__name__, 'resources/img/awacy_running.png'))
-            elif game.awacy_status == AWACYStatus.BROKEN:
-                lblicon.setToolTip(self.tr('Anti-Cheat stops game from running properly'))
-                p.loadFromData(pkgutil.get_data(__name__, 'resources/img/awacy_broken.png'))
-            elif game.awacy_status == AWACYStatus.DENIED:
-                lblicon.setToolTip(self.tr('Linux support was explicitly denied'))
-                p.loadFromData(pkgutil.get_data(__name__, 'resources/img/awacy_denied.png'))
-            else:
-                lblicon.setToolTip(self.tr('Anti-Cheat status unknown'))
-                p.loadFromData(pkgutil.get_data(__name__, 'resources/img/awacy_unknown.png'))
+            awacy_tooltip, awacy_icon = self.get_steamapp_awacystatus(game)
+            p.loadFromData(pkgutil.get_data(__name__, os.path.join('resources/img', awacy_icon)))
+            lblicon.setToolTip(awacy_tooltip)
             lblicon.setPixmap(p)
 
-            # Used for sorting
             lblicon_item = QTableWidgetItem()
             lblicon_item.setData(Qt.DisplayRole, game.awacy_status.value)
             lblicon_item.setData(Qt.UserRole, AWACY_WEB_URL)
@@ -252,9 +203,6 @@ class PupguiGameListDialog(QObject):
         self.update_queued_ctools_steam()
         self.ui.close()
 
-    def btn_fetch_protondb_clicked(self, game: SteamApp):
-        get_protondb_status(game, self.protondb_status_fetched)
-
     @Slot(SteamApp)
     def update_protondb_status(self, game: SteamApp):
         """ Slot is gets called when get_protondb_status finishes """
@@ -278,18 +226,16 @@ class PupguiGameListDialog(QObject):
 
     def queue_ctool_change_steam(self, ctool_name: str, game: SteamApp):
         """ add compatibility tool changes to queue (Steam) """
-        if ctool_name in {'-', ''}:
-            ctool_name = None
-        self.queued_changes[game] = ctool_name
+        ctool_name = None if ctool_name in {'-', ''} else ctool_name
 
+        self.queued_changes[game] = ctool_name
         self.ui.tableGames.item(self.ui.tableGames.currentRow(), 1).setData(Qt.DisplayRole, ctool_name)
 
     def update_queued_ctools_steam(self):
         """ update the compatibility tools for all queued games (Steam) """
-        if len(self.queued_changes) == 0:
-            return
-        steam_update_ctools(self.queued_changes, steam_config_folder=self.install_loc.get('vdf_dir'))
-        self.game_property_changed.emit(True)
+        if len(self.queued_changes) > 0:
+            steam_update_ctools(self.queued_changes, steam_config_folder=self.install_loc.get('vdf_dir'))
+            self.game_property_changed.emit(True)
 
     def item_doubleclick_action(self, item):
         """ open link attached for QTableWidgetItem in browser """
@@ -299,3 +245,54 @@ class PupguiGameListDialog(QObject):
         elif isinstance(item_url, Callable):
             item_url(item.text())
 
+    def get_steamapp_awacystatus(self, game: SteamApp) -> Tuple[str, str]:
+        """ Return translated status text and icon representing AreWeAntiCheatYet.com status for a Steam game """
+        awacy_status: str = ''
+        awacy_icon: str = ''
+
+        if game.awacy_status == AWACYStatus.ASUPPORTED:
+            awacy_status = self.tr('Support was explicitly enabled / works out of the box')
+            awacy_icon = 'awacy_supported.png'
+        elif game.awacy_status == AWACYStatus.PLANNED:
+            awacy_status = self.tr('Game plans to support Proton/Wine')
+            awacy_icon = 'awacy_planned.png'
+        elif game.awacy_status == AWACYStatus.RUNNING:
+            awacy_status = self.tr('No official statement but runs fine (may require tinkering)')
+            awacy_icon = 'awacy_running.png'
+        elif game.awacy_status == AWACYStatus.BROKEN:
+            awacy_status = self.tr('Anti-Cheat stops game from running properly')
+            awacy_icon = 'awacy_broken.png'
+        elif game.awacy_status == AWACYStatus.DENIED:
+            awacy_status = self.tr('Linux support was explicitly denied')
+            awacy_status = 'awacy_denied.png'
+        else:
+            awacy_status = self.tr('Anti-Cheat status unknown')
+            awacy_icon = 'awacy_unknown.png'
+
+        return awacy_status, awacy_icon
+
+    def get_steamdeck_compatibility(self, game: SteamApp) -> str:
+        """ Return translated Steam Deck compatibility rating text for a Steam game """
+        deckc = game.get_deck_compat_category()
+        deckt = game.get_deck_recommended_tool()
+
+        if deckc == SteamDeckCompatEnum.UNKNOWN:
+            return self.tr('Unknown')
+        elif deckc == SteamDeckCompatEnum.UNSUPPORTED:
+            return self.tr('Unsupported')
+        elif deckc == SteamDeckCompatEnum.PLAYABLE:
+            if deckt == '':
+                return self.tr('Playable')
+            elif deckt == 'native':
+                return self.tr('Native (playable)')
+            else:
+                return self.tr('Playable using {compat_tool}').format(compat_tool=deckt)
+        elif deckc == SteamDeckCompatEnum.VERIFIED:
+            if deckt == '':
+                return self.tr('Verified')
+            elif deckt == 'native':
+                return self.tr('Native (verified)')
+            else:
+                return self.tr('Verified for {compat_tool}').format(compat_tool=deckt)
+        else:
+            return ''
