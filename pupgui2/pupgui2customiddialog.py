df7eb5dc40c24c7a15f24bf6940237c594fdb9a9
Eamonn Rea <eamonnrea@gmail.com>
2023-04-08 00:34:23 +0100

    CID: Use UI file

--- pupgui2/pupgui2customiddialog.py
+++ pupgui2/pupgui2customiddialog.py
@@ -1,58 +1,60 @@
 import os
+import pkgutil
 
-from PySide6.QtCore import Signal
+from PySide6.QtCore import Signal, QDataStream, QByteArray, QObject
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QDialog, QFileDialog, QLabel, QPushButton, QLineEdit, QComboBox, QFormLayout
+from PySide6.QtWidgets import QFileDialog, QLabel, QPushButton, QLineEdit, QComboBox, QFormLayout
+from PySide6.QtUiTools import QUiLoader
 
 from pupgui2.util import config_custom_install_location
 
 
-class PupguiCustomInstallDirectoryDialog(QDialog):
+class PupguiCustomInstallDirectoryDialog(QObject):
 
     custom_id_set = Signal()
 
     def __init__(self, parent=None):
         super(PupguiCustomInstallDirectoryDialog, self).__init__(parent)
 
+        self.load_ui()
         self.setup_ui()
+        self.ui.show()
+
+        self.ui.setFixedSize(self.ui.size())
+
+    def load_ui(self):
+        data = pkgutil.get_data(__name__, 'resources/ui/pupgui2_custominstalldirectorydialog.ui')
+        ui_file = QDataStream(QByteArray(data))
+        loader = QUiLoader()
+        self.ui = loader.load(ui_file.device())
 
     def setup_ui(self):
-        self.setWindowTitle(self.tr('Custom Install Directory'))
-        self.setModal(True)
-        self.setMinimumSize(320, 120)
+        self.ui.setWindowTitle(self.tr('Custom Install Directory'))
 
-        formLayout = QFormLayout()
-        self.txtInstallDirectory = QLineEdit()
-        self.txtIdBrowseAction = self.txtInstallDirectory.addAction(QIcon.fromTheme('document-open'), QLineEdit.TrailingPosition)
+        self.txtIdBrowseAction = self.ui.txtInstallDirectory.addAction(QIcon.fromTheme('document-open'), QLineEdit.TrailingPosition)
         self.txtIdBrowseAction.triggered.connect(self.txt_id_browse_action_triggered)
-        self.comboLauncher = QComboBox()
-        self.btnSave = QPushButton(self.tr('Save'))
-        formLayout.addRow(QLabel(self.tr('Directory:')), self.txtInstallDirectory)
-        formLayout.addRow(QLabel(self.tr('Launcher:')), self.comboLauncher)
-        formLayout.addWidget(self.btnSave)
-        self.setLayout(formLayout)
 
-        self.txtInstallDirectory.textChanged.connect(self.txt_install_directory_text_changed)
-        self.comboLauncher.addItems([
+        self.ui.txtInstallDirectory.textChanged.connect(self.txt_install_directory_text_changed)
+        self.ui.comboLauncher.addItems([
             'steam',
             'lutris',
             'heroicwine',
             'heroicproton',
             'bottles'
             ])
-        self.btnSave.clicked.connect(self.btn_save_clicked)
 
-        self.show()
+        self.ui.btnSave.clicked.connect(self.btn_save_clicked)
+        self.ui.btnCancel.clicked.connect(self.ui.close)
 
     def txt_install_directory_text_changed(self, text):
         if text.strip() == '':
-            self.btnSave.setText(self.tr('Reset'))
+            self.ui.btnSave.setText(self.tr('Reset'))
         else:
-            self.btnSave.setText(self.tr('Save'))
+            self.ui.btnSave.setText(self.tr('Save'))
 
     def btn_save_clicked(self):
-        install_dir = self.txtInstallDirectory.text().strip()
-        launcher = self.comboLauncher.currentText()
+        install_dir = self.ui.txtInstallDirectory.text().strip()
+        launcher = self.ui.comboLauncher.currentText()
 
         if install_dir == '':
             config_custom_install_location(install_dir='remove')
@@ -63,12 +65,12 @@ class PupguiCustomInstallDirectoryDialog(QDialog):
             print('custom install directory: set to', install_dir)
 
         self.custom_id_set.emit()
-        self.close()
+        self.ui.close()
 
     def txt_id_browse_action_triggered(self):
-        dialog = QFileDialog(self)
+        dialog = QFileDialog(self.ui)
         dialog.setFileMode(QFileDialog.Directory)
         dialog.setOption(QFileDialog.ShowDirsOnly)
         dialog.setDirectory(os.path.expanduser('~'))
-        dialog.fileSelected.connect(self.txtInstallDirectory.setText)
+        dialog.fileSelected.connect(self.ui.txtInstallDirectory.setText)
         dialog.open()
