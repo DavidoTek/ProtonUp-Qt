5ee4bdb02f906d4ff096d514db1afdcde5736c69
Eamonn Rea <eamonnrea@gmail.com>
2024-05-06 08:36:53 +0100

    Custom Install Directory: Show hidden folders by default (#388)

--- pupgui2/pupgui2customiddialog.py
+++ pupgui2/pupgui2customiddialog.py
@@ -1,7 +1,7 @@
 import os
 import pkgutil
 
-from PySide6.QtCore import Signal, QDataStream, QByteArray, QObject
+from PySide6.QtCore import Signal, QDataStream, QByteArray, QObject, QDir
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QFileDialog, QLineEdit
 from PySide6.QtUiTools import QUiLoader
@@ -86,6 +86,7 @@ class PupguiCustomInstallDirectoryDialog(QObject):
         dialog = QFileDialog(self.ui, directory=initial_dir)
         dialog.setFileMode(QFileDialog.Directory)
         dialog.setOption(QFileDialog.ShowDirsOnly)
+        dialog.setFilter(QDir.Dirs | QDir.Hidden | QDir.NoDotAndDotDot)
         dialog.setWindowTitle(self.tr('Select Custom Install Directory — ProtonUp-Qt'))
         dialog.fileSelected.connect(self.ui.txtInstallDirectory.setText)
         dialog.open()
