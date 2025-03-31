8995e67f290fbbc37bec010270e95fe827d96d2f
Eamonn Rea <eamonnrea@gmail.com>
2025-03-31 23:48:06 +0100

    DXVK Nightly: Refactor to use commit hash again instead of workflow ID
    This allows us to display the commit hash in the releases dropdown
    which helps with readibility, and it also allows us to preserve
    the functionality of the `get_info_url` method which relies on
    using the commit hash.

--- pupgui2/resources/ctmods/ctmod_z2dxvknightly.py
+++ pupgui2/resources/ctmods/ctmod_z2dxvknightly.py
@@ -21,9 +21,10 @@ class CtInstaller(DXVKInstaller):
     BUFFER_SIZE: int = 65536
     CT_WORKFLOW_URL: str = 'https://api.github.com/repos/doitsujin/dxvk/actions/workflows'
     CT_ARTIFACT_URL: str = 'https://api.github.com/repos/doitsujin/dxvk/actions/runs/{}/artifacts'
+    CT_ALL_ARTIFACTS_URL: str = 'https://api.github.com/repos/doitsujin/dxvk/actions/artifacts'
     CT_INFO_URL: str = 'https://github.com/doitsujin/dxvk/commit/'
 
-    DXVK_WORKFLOW_NAME = 'artifacts'
+    DXVK_WORKFLOW_NAME: str = 'artifacts'
 
     def __init__(self, main_window = None):
 
@@ -31,36 +32,45 @@ class CtInstaller(DXVKInstaller):
 
         self.release_format: str = 'zip'
 
-    # TODO break out into separate util function and unit test with:
-    #      * happy path
-    #      * 30 successful workflows across pages (expired artifacts that spread them across pages)
-    #      * successful workflows but not enough to mmatch the count (say 15 successful workflows)
-    #      * 0 successful workflows (all failed) 
-    def __fetch_workflows(self, count: int = 30) -> list:
+    def __fetch_workflows(self, count: int = 30) -> list[str]:
 
         """
         Get all active, successful runs in the DXVK Linux-compatible workflow.
         Return Type: list
         """
 
-        tags: list = []
-        for workflow in self.rs.get(f'{self.CT_WORKFLOW_URL}?per_page={str(count)}').json().get("workflows", {}):
+        workflow_request_url: str = f'{self.CT_WORKFLOW_URL}?per_page={str(count)}'
+        workflow_response_json: dict = self.rs.get(workflow_request_url).json()
+
+        tags: list[str] = []
+        for workflow in workflow_response_json.get('workflows', {}):
             if workflow['state'] != "active" or self.DXVK_WORKFLOW_NAME not in workflow['path']:
                 continue
 
             page = 1
-            while page != -1 and page < 5:  # fetch more (up to 5 pages) if first releases all failed
+            while page != -1 and page <= 5:  # fetch more (up to 5 pages) if first releases all failed
                 at_least_one_failed = False  # ensure the reason that len(tags)=0 is that releases failed
-                for run in self.rs.get(workflow['url'] + f'/runs?per_page={str(count)}&page={page}').json()['workflow_runs']:
-                    if run['conclusion'] == "success":
-                        tags.append(str(run['id']))
-                    elif run['conclusion'] == "failure":
+
+                workflow_runs_request_url: str = f'{workflow["url"]}/runs?per_page={count}&page={page}'
+                workflow_runs_response_json: dict = self.rs.get(workflow_runs_request_url).json()
+
+                for run in workflow_runs_response_json.get('workflow_runs', {}):
+                    if run['conclusion'] == "failure":
                         at_least_one_failed = True
+
+                        continue
+
+                    # TODO can make this generic so that i.e. this DXVK Ctmod can use commmit SHAs but Proton-tkg can use workflow IDs?
+                    #      then this could be a generic function shared between ctmods and could be in a util file, unit tested, etc
+                    commit_hash: str = str(run['head_commit']['id'][:7])
+                    tags.append(commit_hash)
 
                 if len(tags) == 0 and at_least_one_failed:
                     page += 1
-                else:
-                    page = -1
+
+                    continue
+
+                page = -1
 
         return tags
 
@@ -73,19 +83,21 @@ class CtInstaller(DXVKInstaller):
 
         return self.__fetch_workflows(count=count)
 
-    # TODO break out into separate util functio and unit test
-    def __get_artifact_url_from_id(self, id: str):
+    def __get_artifact_from_commit(self, commit):
+
         """
-        Get artifact by the workflow run ID.
+        Get artifact from commit
         Return Type: str
         """
-        print(f'{self.CT_ARTIFACT_URL.format(id)}?per_page=100')
 
-        artifact_info = self.rs.get(f'{self.CT_ARTIFACT_URL.format(id)}?per_page=100').json()
-        if artifact_info.get("total_count") != 1:
-            return None
-
-        return artifact_info["artifacts"][0]
+        for artifact in self.rs.get(f'{self.CT_ALL_ARTIFACTS_URL}?per_page=100').json()["artifacts"]:
+            # DXVK appends '-msvc-output' to Windows builds
+            # See: https://github.com/doitsujin/dxvk/blob/20a6fae8a7f60e7719724b229552eba1ae6c3427/.github/workflows/test-build-windows.yml#L80
+            if artifact['workflow_run']['head_sha'][:len(commit)] == commit and not artifact['name'].endswith('-msvc-output'):
+                artifact['workflow_run']['head_sha'] = commit
+                return artifact
+        
+        return None
 
     def __fetch_github_data(self, tag: str):
 
@@ -96,8 +108,8 @@ class CtInstaller(DXVKInstaller):
             'version', 'date', 'download', 'size'
         """
 
-        # Tag in this case is the workflow run ID
-        data = self.__get_artifact_url_from_id(tag)
+        # Tag in this case is the commit hash
+        data = self.__get_artifact_from_commit(tag)
         if not data:
             return
         values = {'version': data['workflow_run']['head_sha'][:7], 'date': data['updated_at'].split('T')[0]}
