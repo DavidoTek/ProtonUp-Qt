a5106f07eae9086e0d8d27ee7275f8096dd092fd
Eamonn Rea <eamonnrea@gmail.com>
2025-03-31 23:02:44 +0100

    DXVK Nightly: Fix fetching Windows MSVC Builds

--- pupgui2/resources/ctmods/ctmod_z2dxvknightly.py
+++ pupgui2/resources/ctmods/ctmod_z2dxvknightly.py
@@ -19,8 +19,11 @@ CT_DESCRIPTION = {'en': QCoreApplication.instance().translate('ctmod_z2dxvknight
 class CtInstaller(DXVKInstaller):
 
     BUFFER_SIZE: int = 65536
-    CT_URL: str = 'https://api.github.com/repos/doitsujin/dxvk/actions/artifacts'
+    CT_WORKFLOW_URL: str = 'https://api.github.com/repos/doitsujin/dxvk/actions/workflows'
+    CT_ARTIFACT_URL: str = 'https://api.github.com/repos/doitsujin/dxvk/actions/runs/{}/artifacts'
     CT_INFO_URL: str = 'https://github.com/doitsujin/dxvk/commit/'
+
+    DXVK_WORKFLOW_NAME = 'artifacts'
 
     def __init__(self, main_window = None):
 
@@ -28,39 +31,61 @@ class CtInstaller(DXVKInstaller):
 
         self.release_format: str = 'zip'
 
-    def fetch_releases(self, count: int = 100, page: int = 1):
+    # TODO break out into separate util function and unit test with:
+    #      * happy path
+    #      * 30 successful workflows across pages (expired artifacts that spread them across pages)
+    #      * successful workflows but not enough to mmatch the count (say 15 successful workflows)
+    #      * 0 successful workflows (all failed) 
+    def __fetch_workflows(self, count: int = 30) -> list:
 
         """
-        List available releases
-        Return Type: str[]
+        Get all active, successful runs in the DXVK Linux-compatible workflow.
+        Return Type: list
         """
 
-        # TODO no longer correct because DXVK has two workflows, see #500
-        tags = []
-
-        for artifact in ghapi_rlcheck(self.rs.get(f'{self.CT_URL}?per_page={count}&page={page}').json()).get("artifacts", {}):
-            workflow = artifact['workflow_run']
-            if workflow["head_branch"] != "master" or artifact["expired"]:
+        tags: list = []
+        for workflow in self.rs.get(f'{self.CT_WORKFLOW_URL}?per_page={str(count)}').json().get("workflows", {}):
+            if workflow['state'] != "active" or self.DXVK_WORKFLOW_NAME not in workflow['path']:
                 continue
-            tags.append(workflow['head_sha'][:7])
+
+            page = 1
+            while page != -1 and page < 5:  # fetch more (up to 5 pages) if first releases all failed
+                at_least_one_failed = False  # ensure the reason that len(tags)=0 is that releases failed
+                for run in self.rs.get(workflow['url'] + f'/runs?per_page={str(count)}&page={page}').json()['workflow_runs']:
+                    if run['conclusion'] == "success":
+                        tags.append(str(run['id']))
+                    elif run['conclusion'] == "failure":
+                        at_least_one_failed = True
+
+                if len(tags) == 0 and at_least_one_failed:
+                    page += 1
+                else:
+                    page = -1
 
         return tags
 
-    def __get_artifact_from_commit(self, commit: str):
+    def fetch_releases(self, count: int = 30, page: int = 1) -> list[str]:
 
         """
-        Get artifact from commit
+        List available releases.
+        Return Type: str[]
+        """
+
+        return self.__fetch_workflows(count=count)
+
+    # TODO break out into separate util functio and unit test
+    def __get_artifact_url_from_id(self, id: str):
+        """
+        Get artifact by the workflow run ID.
         Return Type: str
         """
+        print(f'{self.CT_ARTIFACT_URL.format(id)}?per_page=100')
 
-        # TODO this may also need changed as a result of #500
-        for artifact in self.rs.get(f'{self.CT_URL}?per_page=100').json()["artifacts"]:
-            if artifact['workflow_run']['head_sha'][:len(commit)] == commit:
-                artifact['workflow_run']['head_sha'] = commit
+        artifact_info = self.rs.get(f'{self.CT_ARTIFACT_URL.format(id)}?per_page=100').json()
+        if artifact_info.get("total_count") != 1:
+            return None
 
-                return artifact
-
-        return None
+        return artifact_info["artifacts"][0]
 
     def __fetch_github_data(self, tag: str):
 
@@ -71,8 +96,8 @@ class CtInstaller(DXVKInstaller):
             'version', 'date', 'download', 'size'
         """
 
-        # Tag in this case is the commit hash.
-        data = self.__get_artifact_from_commit(tag)
+        # Tag in this case is the workflow run ID
+        data = self.__get_artifact_url_from_id(tag)
         if not data:
             return
         values = {'version': data['workflow_run']['head_sha'][:7], 'date': data['updated_at'].split('T')[0]}
@@ -98,6 +123,7 @@ class CtInstaller(DXVKInstaller):
         return (data, dxvk_dir)
 
     def get_info_url(self, version: str) -> str:
+
         """
         Get link with info about version (eg. GitHub release page)
         Return Type: str
