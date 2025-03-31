d4b4e6acb5b90f1eed92db42d5348ab94f5ed35e
Eamonn Rea <eamonnrea@gmail.com>
2025-03-31 22:18:18 +0100

    DXVK Nightly: Simplify by Inheriting DXVK

--- pupgui2/resources/ctmods/ctmod_z2dxvknightly.py
+++ pupgui2/resources/ctmods/ctmod_z2dxvknightly.py
@@ -3,12 +3,12 @@
 # Copyright (C) 2022 DavidoTek, partially based on AUNaseef's protonup
 
 import os
-import requests
 
-from PySide6.QtCore import QObject, QCoreApplication, Signal, Property
+from PySide6.QtCore import QCoreApplication
 
-from pupgui2.util import ghapi_rlcheck, extract_zip
-from pupgui2.util import build_headers_with_authorization
+from pupgui2.util import ghapi_rlcheck
+
+from pupgui2.resources.ctmods.ctmod_z0dxvk import CtInstaller as DXVKInstaller
 
 
 CT_NAME = 'DXVK (nightly)'
@@ -16,85 +16,61 @@ CT_LAUNCHERS = ['lutris', 'advmode']
 CT_DESCRIPTION = {'en': QCoreApplication.instance().translate('ctmod_z2dxvknightly', '''Nightly version of DXVK (master branch), a Vulkan based implementation of Direct3D 8, 9, 10 and 11 for Linux/Wine.<br/><br/><b>Warning: Nightly version is unstable, use with caution!</b>''')}
 
 
-class CtInstaller(QObject):
+class CtInstaller(DXVKInstaller):
 
-    BUFFER_SIZE = 65536
-    CT_URL = 'https://api.github.com/repos/doitsujin/dxvk/actions/artifacts'
-    CT_INFO_URL = 'https://github.com/doitsujin/dxvk/commit/'
-
-    p_download_progress_percent = 0
-    download_progress_percent = Signal(int)
+    BUFFER_SIZE: int = 65536
+    CT_URL: str = 'https://api.github.com/repos/doitsujin/dxvk/actions/artifacts'
+    CT_INFO_URL: str = 'https://github.com/doitsujin/dxvk/commit/'
 
     def __init__(self, main_window = None):
-        super(CtInstaller, self).__init__()
-        self.p_download_canceled = False
 
-        self.rs = requests.Session()
-        rs_headers = build_headers_with_authorization({}, main_window.web_access_tokens, 'github')
-        self.rs.headers.update(rs_headers)
+        super().__init__(main_window)
 
-    def get_download_canceled(self):
-        return self.p_download_canceled
+        self.release_format: str = 'zip'
 
-    def set_download_canceled(self, val):
-        self.p_download_canceled = val
+    def fetch_releases(self, count: int = 100, page: int = 1):
 
-    download_canceled = Property(bool, get_download_canceled, set_download_canceled)
-
-    def __set_download_progress_percent(self, value : int):
-        if self.p_download_progress_percent == value:
-            return
-        self.p_download_progress_percent = value
-        self.download_progress_percent.emit(value)
-
-    def __download(self, url, destination, f_size):
-        # f_size in argumentbecause artifacts don't have Content-Length.
         """
-        Download files from url to destination
-        Return Type: bool
+        List available releases
+        Return Type: str[]
         """
-        try:
-            file = requests.get(url, stream=True)
-        except OSError:
-            return False
 
-        self.__set_download_progress_percent(1) # 1 download started
-        c_count = int(f_size / self.BUFFER_SIZE)
-        c_current = 1
-        destination = os.path.expanduser(destination)
-        os.makedirs(os.path.dirname(destination), exist_ok=True)
-        with open(destination, 'wb') as dest:
-            for chunk in file.iter_content(chunk_size=self.BUFFER_SIZE):
-                if self.download_canceled:
-                    self.download_canceled = False
-                    self.__set_download_progress_percent(-2) # -2 download canceled
-                    return False
-                if chunk:
-                    dest.write(chunk)
-                    dest.flush()
-                self.__set_download_progress_percent(int(min(c_current / c_count * 98.0, 98.0))) # 1-98, 100 after extract
-                c_current += 1
-        self.__set_download_progress_percent(99) # 99 download complete
-        return True
+        # TODO no longer correct because DXVK has two workflows, see #500
+        tags = []
 
-    def __get_artifact_from_commit(self, commit):
+        for artifact in ghapi_rlcheck(self.rs.get(f'{self.CT_URL}?per_page={count}&page={page}').json()).get("artifacts", {}):
+            workflow = artifact['workflow_run']
+            if workflow["head_branch"] != "master" or artifact["expired"]:
+                continue
+            tags.append(workflow['head_sha'][:7])
+
+        return tags
+
+    def __get_artifact_from_commit(self, commit: str):
+
         """
         Get artifact from commit
         Return Type: str
         """
+
+        # TODO this may also need changed as a result of #500
         for artifact in self.rs.get(f'{self.CT_URL}?per_page=100').json()["artifacts"]:
             if artifact['workflow_run']['head_sha'][:len(commit)] == commit:
                 artifact['workflow_run']['head_sha'] = commit
+
                 return artifact
+
         return None
 
-    def __fetch_github_data(self, tag):
+    def __fetch_github_data(self, tag: str):
+
         """
         Fetch GitHub release information
         Return Type: dict
         Content(s):
-            'version', 'date', 'download', 'size', 'checksum'
+            'version', 'date', 'download', 'size'
         """
+
         # Tag in this case is the commit hash.
         data = self.__get_artifact_from_commit(tag)
         if not data:
@@ -105,50 +81,26 @@ class CtInstaller(QObject):
         values['size'] = data['size_in_bytes']
         return values
 
-    def is_system_compatible(self):
-        """
-        Are the system requirements met?
-        Return Type: bool
-        """
-        return True
+    def __get_data(self, version: str, install_dir: str) -> tuple[dict | None, str | None]:
 
-    def fetch_releases(self, count=100, page=1):
         """
-        List available releases
-        Return Type: str[]
+        Get needed download data and path to extract directory.
+        Return Type: diple[dict | None, str | None]
         """
-        tags = []
-        for artifact in ghapi_rlcheck(self.rs.get(f'{self.CT_URL}?per_page={count}&page={page}').json()).get("artifacts", {}):
-            workflow = artifact['workflow_run']
-            if workflow["head_branch"] != "master" or artifact["expired"]:
-                continue
-            tags.append(workflow['head_sha'][:7])
-        return tags
 
-    def get_tool(self, version, install_dir, temp_dir):
-        """
-    Download and install the compatibility tool
-        Return Type: bool
-        """
         data = self.__fetch_github_data(version)
-        if not data or 'download' not in data:
-            return False
+        if not data or not 'download' in data:
+            return (None, None)
 
-        dxvk_zip = os.path.join(temp_dir, data['download'].split('/')[-1])
-        if not self.__download(url=data['download'], destination=dxvk_zip, f_size=data['size']):
-            return False
-
+        # TODO This is hardcoded to Lutris as DXVK Nightly currently doesn't support any other launchers -- Could possibly add support for Heroic in future
         dxvk_dir = os.path.join(install_dir, '../../runtime/dxvk', 'dxvk-git-' + data['version'])
-        if not extract_zip(dxvk_zip, dxvk_dir):
-            return False
 
-        self.__set_download_progress_percent(100)
+        return (data, dxvk_dir)
 
-        return True
-
-    def get_info_url(self, version):
+    def get_info_url(self, version: str) -> str:
         """
         Get link with info about version (eg. GitHub release page)
         Return Type: str
         """
+
         return self.CT_INFO_URL + version
