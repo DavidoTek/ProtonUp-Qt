dbf0aa127af5a914ceec5df972968620fd421bb4
Eamonn Rea <eamonnrea@gmail.com>
2025-04-08 01:19:08 +0100

    util: Allow `fetch_project_releases` to take a conditional for adding assets
    This allows for ctmods, such as Proton-Sarek, to specify how to include
    additional assets that may not conform to the tagged release name.

--- pupgui2/util.py
+++ pupgui2/util.py
@@ -609,7 +609,7 @@ def is_online(host: str = 'https://api.github.com/rate_limit/', timeout: int = 5
 
 
 # Only used for dxvk and dxvk-async right now, but is potentially useful to more ctmods?
-def fetch_project_releases(releases_url: str, rs: requests.Session, count=100, page=1) -> list[str]:
+def fetch_project_releases(releases_url: str, rs: requests.Session, count: int = 100, page: int = 1, include_extra_asset: Callable | None = None) -> list[str]:
 
     """
     List available releases for a given project URL hosted using requests.
@@ -628,7 +628,21 @@ def fetch_project_releases(releases_url: str, rs: requests.Session, count=100, p
     else:
         return []  # Unknown API, cannot fetch releases!
 
-    return [release[tag_key] for release in releases if tag_key in release]
+    releases_list: list[str] = []
+    for release in releases:
+        if tag_key in release:
+            releases_list.append(release[tag_key])
+
+        if 'assets' not in release or len(release['assets']) <= 0:
+            continue
+
+        if not include_extra_asset:
+            continue
+
+        if extra_asset := include_extra_asset(release):
+            releases_list.append(extra_asset)
+
+    return releases_list
 
 
 def get_assets_from_release(release_url: str, release: dict) -> dict:
