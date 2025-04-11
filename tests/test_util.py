e2857969666acf75374a80eedac1f1ba1dabeac8
Eamonn Rea <eamonnrea@gmail.com>
2025-04-11 21:22:09 +0100

    test_util: Parametrize `test_get_launcher_from_installdir`

--- tests/test_util.py
+++ tests/test_util.py
@@ -76,38 +76,44 @@ def test_get_dict_key_from_value(test_dict: dict[str | int | Launcher, str | str
         assert type(retrieved_key) is expected_type
 
 
-def test_get_launcher_from_installdir() -> None:
+@pytest.mark.parametrize(
+    'launcher_paths, expected_launcher', [
+        pytest.param(
+            [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'steam' ],
+            Launcher.STEAM,
+            id='Steam Launcher'
+        ),
+        pytest.param(
+            [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'lutris' ],
+            Launcher.LUTRIS,
+            id='Lutris Launcher'
+        ),
+        pytest.param(
+            [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] in ('heroicwine', 'heroicproton') ],
+            Launcher.HEROIC,
+            id='Heroic Wine / Heroic Proton Launcher'
+        ),
+        pytest.param(
+            [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'bottles' ],
+            Launcher.BOTTLES,
+            id='Bottles Launcher'
+        ),
+        pytest.param(
+            [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'winezgui' ],
+            Launcher.WINEZGUI,
+            id='WineZGUI Launcher'
+        ),
+    ]
+)
+def test_get_launcher_from_installdir(launcher_paths: list[str], expected_launcher: Launcher) -> None:
 
     """
     Test whether get_laucher_from_installdir returns the correct Launcher type Enum from the installdir path.
     """
 
-    # All possible Steam paths
-    steam_install_paths: list[str] = [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'steam' ]
-    steam_install_path_tests: list[Launcher] = [ get_launcher_from_installdir(steam_path) for steam_path in steam_install_paths ]
+    launcher_from_installdir: list[Launcher] = [ get_launcher_from_installdir(launcher_path) for launcher_path in launcher_paths ]
 
-    # All possible Lutris paths
-    lutris_install_paths: list[str] = [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'lutris' ]
-    lutris_install_path_tests: list[Launcher] = [ get_launcher_from_installdir(lutris_path) for lutris_path in lutris_install_paths ]
-
-    # All possible Heroic paths
-    heroic_install_paths: list[str] = [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] in ['heroicwine', 'heroicproton'] ]
-    heroic_install_path_tests: list[Launcher] = [ get_launcher_from_installdir(heroic_path) for heroic_path in heroic_install_paths ]
-
-    # All possible Bottles paths
-    bottles_install_paths: list[str] = [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'bottles' ]
-    bottles_install_path_tests: list[Launcher] = [ get_launcher_from_installdir(bottles_path) for bottles_path in bottles_install_paths ]
-
-    # All possible WineZGUI paths
-    winezgui_install_paths: list[str] = [ install_location['install_dir'] for install_location in POSSIBLE_INSTALL_LOCATIONS if install_location['launcher'] == 'winezgui' ]
-    winezgui_install_path_tests: list[Launcher] = [ get_launcher_from_installdir(winezgui_path) for winezgui_path in winezgui_install_paths ]
-
-
-    assert all(launcher == Launcher.STEAM for launcher in steam_install_path_tests)
-    assert all(launcher == Launcher.LUTRIS for launcher in lutris_install_path_tests)
-    assert all(launcher == Launcher.HEROIC for launcher in heroic_install_path_tests)
-    assert all(launcher == Launcher.BOTTLES for launcher in bottles_install_path_tests)
-    assert all(launcher == Launcher.WINEZGUI for launcher in winezgui_install_path_tests)
+    assert all(launcher == expected_launcher for launcher in launcher_from_installdir)
 
 
 def test_get_random_game_name() -> None:
