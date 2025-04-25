16b05f8f1ae3093827e049e2d44775e4b5de58f8
Eamonn Rea <eamonnrea@gmail.com>
2025-04-25 15:47:03 +0100

    test_dbusutil: Add tests for dbusutil functions

---
+++ tests/test_dbusutil.py
@@ -0,0 +1,91 @@
+import pytest
+
+from pytest_mock import MockerFixture
+
+from PySide6.QtDBus import QDBusMessage, QDBusConnection
+
+from pupgui2.dbusutil import *
+from pupgui2.constants import DBUS_DOWNLOAD_OBJECT_BASEPATH
+
+
+# NOTE: WOOHOO this mostly works!
+
+
+def test_create_and_send_dbus_message(mocker: MockerFixture) -> None:
+
+    """
+    Given a valid DBus bus Connection,
+    When a message is constrtucted,
+    It should send the constructed message on the given bus.
+    """
+
+    session_bus_mock = mocker.spy(QDBusConnection, 'sessionBus')
+    
+    session_bus_send_mock = mocker.patch('PySide6.QtDBus.QDBusConnection.send')
+    session_bus_send_mock.return_value = True
+
+    # TODO parametrize?
+    dbus_args: dict[str, str | list[str | dict[str, str]]] = {
+        'object': 'echo',
+        'interface': 'net.davidotek.pupgui2.Test',
+        'signal_name': 'Test',
+        'arguments': [
+            'application://net.davidotek.pupgui2.Test',
+            {
+                'test-value': '1'
+            }
+        ]
+    }
+
+    result: bool = create_and_send_dbus_message(
+        str(dbus_args['object']),
+        str(dbus_args['interface']),
+        str(dbus_args['signal_name']),
+        dbus_args['arguments']
+    )
+
+    session_bus_send_mock_message_arg: QDBusMessage = session_bus_send_mock.call_args[0][0]
+
+    assert result
+
+    session_bus_mock.assert_called_once()
+
+    session_bus_send_mock.assert_called_once()
+    
+    assert session_bus_send_mock.return_value
+    
+    assert isinstance(session_bus_send_mock_message_arg, QDBusMessage)
+
+    assert session_bus_send_mock_message_arg.path() == os.path.join(DBUS_DOWNLOAD_OBJECT_BASEPATH, str(dbus_args['object']))
+    assert session_bus_send_mock_message_arg.interface() == dbus_args['interface']
+    assert session_bus_send_mock_message_arg.member() == dbus_args['signal_name']
+    assert session_bus_send_mock_message_arg.arguments() == dbus_args['arguments']
+
+
+def test_create_and_send_dbus_message_bus_not_connected(mocker: MockerFixture) -> None:
+
+    """
+    Given that `DBusConnection` fails to any event bus,
+    When the connection query for `isConnected()` is `False`,
+    Then we should return `False`,
+    And not send a message.
+    """
+
+    session_bus_isConnected_mock = mocker.patch('PySide6.QtDBus.QDBusConnection.isConnected')
+    session_bus_isConnected_mock.return_value = False
+
+    session_bus_send_mock = mocker.patch('PySide6.QtDBus.QDBusConnection.send')
+    session_bus_send_mock.return_value = False
+
+    result: bool = create_and_send_dbus_message('', '', '', [])
+
+    assert not result
+    
+    session_bus_isConnected_mock.assert_called_once()
+
+    session_bus_send_mock.assert_not_called()
+
+
+def test_dbus_progress_message() -> None:
+
+    pass
